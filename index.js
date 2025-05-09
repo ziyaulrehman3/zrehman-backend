3202ee9c077038d1a42bf54255b4b62094b99b8f
Ziya Ul Rehman <farhangolu007@gmail.com>
2025-05-09 17:52:46 +0530

    vercal error fix again

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-app.listen("3000", () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is Running..");
 });
+
+export default app;
